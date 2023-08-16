38aeacceaeb89f6d4c89f7065b81e3e0f23b2848
hamza boujid <hamzaboujid24@gmail.com>
2023-08-16 01:25:35 +0100

    add translation in two pages terms and policy

--- src/components/Terms and conditions/Terms.jsx
+++ src/components/Terms and conditions/Terms.jsx
@@ -2,92 +2,40 @@ import React from 'react';
 import './TermsStyles.css';
 import NavbarTwo from '../navbarForTalk&Bdp/NavbarTwo';
 import FooterTwo from '../footerForTalk&Bdp/FooterTwo';
+import { useTranslation } from 'react-i18next';
 
 function Terms() {
+  const { t } = useTranslation();
   return (
     <div>
       <div name="Terms" className="Terms">
         <NavbarTwo />
         <div className="contentTerms">
-          <h1>Terms and Conditions</h1>
-          <h2>Last revision: 1st of November, 2022</h2>
-          <h2>1. Introduction</h2>
-          <p>This document includes the terms and conditions of use ("Terms of Use") of the platform ("Platform") hosted on the web site https://founderz.com/ ("Website") owned by Founderz Emprendimiento, S.L. ("Founderz" or "We") with registered office at Pineda de Mar, 08397, Barcelona, CIF number B06944375, registered in the Commercial Register of Barcelona, volume 47908, folio 19, sheet B 566041 and contact at hola@founderz.com.
-
-The purpose of these Terms of Use is to regulate the access, navigation and contracting of the various Online Programs offered by Founderz ("Program/s") by those users who have registered on the Platform ("Users") and those who visit the Platform ("Visitors").
-
-By browsing the Platform and/or registering for the Program(s), the User or Visitor accepts these Terms of Use and Privacy Policy . Otherwise, the User or Visitor must cease using the Platform immediately.
-
-Founderz, through the registration to the Program(s), makes available to the Users a training Platform that offers the User access to the selected Program, in microlearning format in online mode from pre-recorded lectures ("Lectures") on the specific subject matter of the respective Program , as well as access to a virtual campus with a variety of additional content of the selected Program including the interactions of the online community (all together the “Services”).</p>
-          <h2>2. How to register?</h2>
-          <h3>a) User Registration</h3>
-          <p>In order to enroll in the Program, the User must register on the Platform, after which he/she will have access to all the content of Conferences and the online community for a period of time in principle unlimited ("Lifetime").
-
-The term defined as Lifetime may be reduced in the event of (i) dissolution or (ii) entry into a liquidation process or (iii) sale (partial or total) of Founderz Emprendimiento, S.L., or (iv) when Founderz considers it in its sole discretion. The aforementioned reduction of the term will in no case generate an economic compensation in favor of the User. In any case Founderz will provide the User access to the content of the respective Program for a minimum period of 1 year from registration.
-
-Once the User registers for the Program, he/she must complete the bank details required to proceed with the payment of the Services, as set forth in the "Payment Method" section, and must register his/her User by providing his/her name and surname, e-mail address and password.
-
-Founderz reserves the right to modify the contents accessible on the Website and the Platform (remove or add Conferences), without, however, generally reducing the service. In the event of a substantial reduction in the content of the training, Founderz will refund a proportional part of the Price (see below).</p>
-          <h3>b) Price</h3>
-          <p>The price of each Program will be as defined in the "Our Programs" section of the Website and will always be expressed in Euro currency and including applicable VAT, which will allow the User to access all the training content, as well as the online community. In addition, the amount may be subject to discounts offered by Founderz, for which Founderz will provide the applicable discount code for each promotion.</p>
-          <h3>c) Payment method</h3>
-          <p>The transmission of personal or banking data is conducted using a secure environment, a server based on the standard security technology SSL (Secure Sockets Layer). All information transmitted by the User travels encrypted through the network.
-
-Likewise, the data entered by the User about his/her credit or debit card are entered directly to Stripe Inc. (payment gateway) and, therefore, are not entered or registered in any Founderz server, and will be treated according to Stripe's legal conditions.
-
-When paying by credit or with debit card, you will always be asked for the following information: the card number, the expiration date, and a Validation Code that matches the last three digits of the number printed in italics on the back of your card, thus offering more guarantees about the security of the transaction. This form of payment is valid only on the website.
-
-Once the requested bank details have been entered in the Stripe platform, payment for the contracted services will be made in a single payment.
-
-When the amount of a purchase has been fraudulently or improperly charged using the number of a payment card, the cardholder may demand the immediate cancellation of the charge. In this case, the corresponding debit entries and re-credit the accounts of the supplier and the cardholder will be made as soon as possible.</p>
-          <h2>3. User Obligations</h2>
-          <p>The User agrees to make proper and lawful use of the contents and Platform and by way of example, but not limited to, not to use them to (i) engage in illegal activities or contrary to good faith and public order; (ii) cause damage to physical and logical systems Founderz or other users; (iii) attempt to access and, where appropriate, use the email accounts of other users and modify or record data.
-
-You agree that Founderz grants you a non-exclusive right to access the Services and therefore your account is personal and non-transferable.
-
-Specifically, the User assumes the following obligations:
-
-- To make payment in advance of the amount of the Founderz Services effectively in the amount and form indicated in sections 2.b. and 2.c.
-
-- Make good use of the Platform without causing any damage to the physical and logical systems of Founderz without contravening current legislation, good faith and public order or damage the rights and interests of third parties.
-
-- To guarantee the truthfulness and accuracy of the data provided when filling in the forms required to proceed with the contracting of the Services, thus avoiding any damage to Founderz as a result of their incorrectness.
-
-- You must be of legal age to be able to enroll in the Program.
-
-- Make good use of your User profile and your password to access the Platform, committing to make diligent use of them, as well as keep them properly so that they are not available to third parties, communicating in any case to Founderz their loss, theft or possible access by an unauthorized third party, so that Founderz can proceed to the immediate blocking.</p>
-          <h2>4. Right of withdrawal</h2>
-          <p>You acknowledge and agree that, once you have accessed the Program, Founderz is not obliged to offer you a right of withdrawal, in accordance with the provisions of the regulations applicable to the consumer since it applies the exception contemplated in Article 103 paragraph m) of the revised text of the General Law for the Defense of Consumers and Users ("TRLGDCU"), since, once you pay the corresponding price and access the digital contents of the Program that we offer, the Services will be considered executed and, therefore, you will lose the right of withdrawal.</p>
-          <h2>5. End of relationship</h2>
-          <p>If the User, at any time, ceases to agree with these Terms of Use or any changes to these Terms of Use, the User must immediately cease use of the Platform.
-
-If the User wishes to deactivate his or her account on the Platform, he or she should send an email to hola@founderz.com indicating his or her willingness to deactivate the User's account. Once the User's account is deactivated, the User will not be able to access his/her account. For information on the retention of User data, the User should refer to our Privacy Policy.</p>
-          <h2>6. Disclaimer of warranties and liability</h2>
-          <p>Founderz warrants that the Program will be of professional quality with content suitable for its stated purposes, and that Founderz will make it available to Users in a diligent and professional manner and will verify that the content is of quality. However, it does not warrant that the same will be error-free or, after a certain period of time after publication, out of date.
-
-To the extent permitted by applicable law, Founderz is not responsible, in any case, for damages of any nature that may cause, including but not limited to: the lack of availability of the portal; for any incident or technical failure and any loss of data that may result from them, as well as for any situation considered a fortuitous cause or force majeure, despite having taken all necessary technological measures to prevent it.
-
-In no event shall Founderz assume any responsibility for the Program or any other content published by third parties or coming from any link belonging to an external website, nor shall it guarantee the technical availability, quality, reliability, accuracy, comprehensiveness, truthfulness, validity and constitutionality of any material or information contained in any of the contents of the Program, virtual community, hyperlinks or other Internet sites.
-
-Founderz reserves the right to update, modify or delete the information contained in its Platform and/or Website, as well as its configuration or presentation, at any time without assuming any responsibility for it.
-
-In no case shall the Program be considered as professional advice by the teachers involved. Any professional relationship between a teacher and a User must be formalized outside the Platform, without the intermediation of Founderz, and by private contract between those parties. Founderz assumes no responsibility for such professional relationship and its effects, nor for having brought the parties thereto to the attention of the other.</p>
-          <h2>7. Intellectual and Industrial Property Rights</h2>
-          <p>Founderz itself or as an assignee, owns all intellectual and industrial property rights of the Platform, as well as the elements contained therein (including but not limited to trademarks, images, sound, audio, video, or text; logos, color combinations, structure and design, selection of materials used, computer programs necessary for its operation, access and use, etc.).
-
-Pursuant to the provisions of national, European and international regulations on intellectual property, the reproduction, distribution and public communication, including making available, of all or part of the contents of the Platform and specifically the content developed by Founderz, for commercial purposes, in any form and by any technical means, without the prior written authorization of Founderz, are expressly prohibited.
-
-The User expressly agrees not to make or allow any recording, downloading, or "scraping" of the content of the Website and/or Platform but shall be limited to viewing such content on the Site itself.</p>
-          <h2>8. Third party links</h2>
-          <p>Founderz is not responsible for the content posted by third parties or third party web pages that can be accessed from the Website; nor for the accuracy, veracity and validity of the information that is not of its own creation, and whose content belongs to third parties outside Founderz.
-
-However, pursuant to Article 17 of Law 34/2002 of July 11, 2002, on information society services and electronic commerce, when Founderz becomes aware of the unlawfulness of such content or links, it will proceed to remove them, as well as blocking the content that may be unlawful or violate the rights of third parties, under Articles 11 and 16 of that Law.</p>
-          <h2>9. Jurisdiction and out-of-court dispute resolution</h2>
-          <p>Founderz reserves the right to file civil and/or criminal actions it deems appropriate for the improper use of its Website and/or Platform and contents or for the breach of these conditions.
-
-Without prejudice to the option available to the parties to voluntarily submit to the online dispute resolution system mentioned in the following section, the relationship between the User and Founderz shall be governed by the regulations in force and applicable in the Spanish territory. If any dispute arises, the parties may submit their disputes to arbitration or go to the ordinary jurisdiction in compliance with the rules on jurisdiction and competence in this regard, with submission to the forum of the consumer's domicile.
-
-Notwithstanding the foregoing, in accordance with Regulation (EU) 524/2013, the European Commission provides a free access platform for out-of-court online dispute resolution for electronic contracting (http://ec.europa.eu/consumers/odr/) to which the parties may voluntarily submit, through the intervention of the so-called Dispute Resolution Body, which acts as a neutral intermediary between the two parties.</p>
+          <h1>{t('Terms&Conditions.title')}</h1>
+          <h2>{t('Terms&Conditions.titleText')}</h2>
+          <h2>{t('Terms&Conditions.Text')}</h2>
+          <p>{t('Terms&Conditions.textTitle1')}</p>
+          <h2>{t('Terms&Conditions.Text1')}</h2>
+          <h3>{t('Terms&Conditions.Text2')}</h3>
+          <p>{t('Terms&Conditions.textTitle2')}</p>
+          <h3>{t('Terms&Conditions.text3')}</h3>
+          <p>{t('Terms&Conditions.textTitle3')}</p>
+          <h3>{t('Terms&Conditions.text4')}</h3>
+          <p>{t('Terms&Conditions.textTitle4')}</p>
+          <h2>{t('Terms&Conditions.text5')}</h2>
+          <p>{t('Terms&Conditions.textTitle5')}</p>
+          <h2>{t('Terms&Conditions.text6')}</h2>
+          <p>{t('Terms&Conditions.textTitle6')}</p>
+          <h2>{t('Terms&Conditions.text7')}</h2>
+          <p>{t('Terms&Conditions.textTitle7')}</p>
+          <h2>{t('Terms&Conditions.text8')}</h2>
+          <p>{t('Terms&Conditions.textTitle8')}</p>
+          <h2>{t('Terms&Conditions.text9')}</h2>
+          <p>{t('Terms&Conditions.textTitle9')}</p>
+          <h2>{t('Terms&Conditions.text10')}</h2>
+          <p>{t('Terms&Conditions.textTitle10')}</p>
+          <h2>{t('Terms&Conditions.text11')}</h2>
+          <p>{t('Terms&Conditions.textTitle11')}</p>
         </div>
       </div>
       <FooterTwo />
